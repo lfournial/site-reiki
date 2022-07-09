d011564621f56d9e90845158356ad7d78fdbaf52
lfournial <lfournial@gmail.com>
2022-07-09 13:18:42 +0200

    change SEO attributes

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,9 +4,15 @@ const Rokstar = ({ Component, pageProps }) => {
     return (
         <>
             <Head>
-                <meta name="description" content="Ô Soin de Soi - Virginie Fournial, Accompagnante au bien être par le Reiki et le massage énergétique à Seclin / Lille - Nord - France. " />
+                <meta name="description" content="Virginie Fournial, Accompagnante au bien être par le Reiki et le massage énergétique à Seclin / Lille / France- Ô Soin de Soi" />
                 <meta name="keywords" content="reiki, massage, énergie, accompagnement, bien etre, Seclin, Lille,Virginie Fournial, Fournial, Virginie" />
-                <title>Virginie Fournial, Accompagnante au bien être par le Reiki et le massage énergétique à Seclin / Lille - Ô Soin de Soi</title>            </Head>
+                <title>Virginie Fournial, Accompagnante au bien être par le Reiki et le massage énergétique à Seclin / Lille - Ô Soin de Soi</title>
+                <link rel="icon" href="/favicon.ico" />
+                <meta property="og:title" content="Virginie Fournial, Accompagnante au bien être par le Reiki et le massage énergétique à Seclin / Lille / France - Ô Soin de Soi" />
+                <meta property="og:description" content="Virginie Fournial, Accompagnante au bien être par le Reiki et le massage énergétique à Seclin / Lille / France - Ô Soin de Soi" />
+                <meta property="og:url" content="https://www.osoin-desoi.fr" />
+                <meta property="og:type" content="website" />
+            </Head>
             <Component {...pageProps} />
         </>
     )
