53e999044a2a8c20d8921122458c6ab7ef372434
lfournial <lfournial@gmail.com>
2022-07-09 08:34:22 +0200

    change SEO attributes

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,9 +1,9 @@
-const SectionTitle = ({title, className}) => {
+const SectionTitle = ({ title, className }) => {
     return (
         <div className={`mb-12 ${className ?? ""}`}>
-            <h2 className="text-3xl font-heading tracking-normal text-dark pb-4 relative before:absolute before:left-0 before:bottom-0 before:h-[5px] before:w-[55px] before:bg-dark after:absolute after:left-0 after:bottom-[2px] after:h-[1px] after:w-[255px] after:bg-dark">
+            <h1 className="text-3xl font-heading tracking-normal text-dark pb-4 relative before:absolute before:left-0 before:bottom-0 before:h-[5px] before:w-[55px] before:bg-dark after:absolute after:left-0 after:bottom-[2px] after:h-[1px] after:w-[255px] after:bg-dark">
                 {title}
-            </h2>
+            </h1>
         </div>
     );
 };
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,10 +4,9 @@ const Rokstar = ({ Component, pageProps }) => {
     return (
         <>
             <Head>
-                <meta name="description" content="Ô Soin de Soi, Virginie Fournial, Accompagnante au bien être par le Reiki et le massage énergétique à Seclin / Lille - Nord - France. " />
-                <title>Ô Soin de Soi</title>
-
-            </Head>
+                <meta name="description" content="Ô Soin de Soi - Virginie Fournial, Accompagnante au bien être par le Reiki et le massage énergétique à Seclin / Lille - Nord - France. " />
+                <meta name="keywords" content="reiki, massage, énergie, accompagnement, bien etre, Seclin, Lille,Virginie Fournial, Fournial, Virginie" />
+                <title>Virginie Fournial, Accompagnante au bien être par le Reiki et le massage énergétique à Seclin / Lille - Ô Soin de Soi</title>            </Head>
             <Component {...pageProps} />
         </>
     )
