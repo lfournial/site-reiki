8c70ba3a42fd2812ec8e69baeb0b822469c44559
lfournial <lfournial@gmail.com>
2022-07-20 18:53:47 +0200

    review meta

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,12 +4,12 @@ const Rokstar = ({ Component, pageProps }) => {
     return (
         <>
             <Head>
-                <meta name="description" content="Virginie Fournial, Accompagnante au bien être par le Reiki et le massage énergétique à Seclin / Lille / France- Ô Soin de Soi" />
-                <meta name="keywords" content="reiki, massage, énergie, accompagnement, bien etre, Seclin, Lille,Virginie Fournial, Fournial, Virginie" />
+                <meta name="description" content="Ô Soin de Soi - Virginie Fournial - Reiki - Massage énergétique - Accompagnante au bien être à Seclin" />
+                <meta name="keywords" content="reiki, massage, relaxation, lâcher prise, énergie, accompagnement, bien etre, Seclin, Lille,Virginie Fournial, Fournial, Virginie, osoin-desoi" />
                 <title>Virginie Fournial, Accompagnante au bien être par le Reiki et le massage énergétique à Seclin / Lille - Ô Soin de Soi</title>
                 <link rel="icon" href="/favicon.ico" />
-                <meta property="og:title" content="Virginie Fournial, Accompagnante au bien être par le Reiki et le massage énergétique à Seclin / Lille / France - Ô Soin de Soi" />
-                <meta property="og:description" content="Virginie Fournial, Accompagnante au bien être par le Reiki et le massage énergétique à Seclin / Lille / France - Ô Soin de Soi" />
+                <meta property="og:title" content="Ô Soin de Soi - Virginie Fournial - Reiki - Massage énergétique - Accompagnante au bien être à Seclin" />
+                <meta property="og:description" content="Ô Soin de Soi - Virginie Fournial - Reiki - Massage énergétique - Accompagnante au bien être à Seclin" />
                 <meta property="og:url" content="https://www.osoin-desoi.fr" />
                 <meta property="og:type" content="website" />
             </Head>
