7d283ab19b549548cbde1badb26a416b6f0dc272
lfournial <lfournial@gmail.com>
2022-08-31 15:10:29 +0200

    remove holidays

--- src/components/HeroArea.jsx
+++ src/components/HeroArea.jsx
@@ -24,13 +24,14 @@ const HeroArea = () => {
 
                 <Shape className="hidden md:block" />
             </section>
+            {/*
             <div
                 className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
             >
                 <div className="relative w-auto my-6 mx-auto max-w-xs">
-                    {/*content*/}
+                    
                     <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                        {/*header*/}
+                        
                         <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                             <h3 className="text-xl font-semibold">
                                 <GiPalmTree className="inline text-dark-400 mr-2" />C'est les vacances <GiPalmTree className="inline text-dark-400 mr-2" />
@@ -44,7 +45,6 @@ const HeroArea = () => {
                                 </span>
                             </button>
                         </div>
-                        {/*body*/}
                         <div className="relative p-4 flex-auto">
                             <p className="my-4 text-slate-500 text-lg leading-relaxed">
                                 Je vous retrouve à compter du 7 septembre.
@@ -54,11 +54,12 @@ const HeroArea = () => {
                                 <br /> <br />Passez un bel été.
                             </p>
                         </div>
-                        {/*footer*/}
+                        
 
                     </div>
-                </div>
+                </div>                
             </div>
+            */}
             <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
 
         </>
