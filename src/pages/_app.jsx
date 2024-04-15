93c174cc6215b1a9bdfe4aeff30ef7007ae0904e
lfournial <lfournial@gmail.com>
2024-04-15 20:24:48 +0200

    add new key word for SEO

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,7 +5,7 @@ const Rokstar = ({ Component, pageProps }) => {
         <>
             <Head>
                 <meta name="description" content="Formation et séances de Reiki - Massage bien-être - Seclin 59113 - Virginie Fournial, accompagnante au bien être" />
-                <meta name="keywords" content="reiki, massage, relaxation, lâcher prise, énergie, Formation au Reiki, stage de Reiki, accompagnement, bien etre, Nord de la France, Seclin, Nord, Lille,Virginie Fournial, Fournial, Virginie, osoin-desoi" />
+                <meta name="keywords" content="reiki, massage, relaxation, lâcher prise, énergie, Formation au Reiki, stage de Reiki, accompagnement, bien etre, 59113, Nord de la France, Seclin, Nord, Lille,Virginie Fournial, Fournial, Virginie, osoin-desoi" />
                 <title>Virginie Fournial, Accompagnante au bien être par le Reiki et le massage bien-être à Seclin / Lille - Ô Soin de Soi</title>
                 <link rel="icon" href="/favicon.ico" />
                 <meta property="og:title" content="Reiki - Massage bien être et formation au Reiki - Seclin / Lille Nord de la France - Virginie Fournial, acccompagnante au bien être" />
